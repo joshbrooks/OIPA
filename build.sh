fb8e3f8d2a6863d1976f4afa978405e2ce34f3e1
Joshua Brooks <josh.vdbroek@gmail.com>
2018-10-29 10:26:45 +0800

    minor tweaks on buildscript

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-docker build --build-arg HOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+') -t oipa -f oipa-server.Dockerfile .
+#!/bin/bash
+HOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')
+container=oipa
+
+docker build --build-arg HOST=${HOST} -t ${container} -f oipa-server.Dockerfile .
