3cb6fc39e70a92021bbc52b28de341131a7e41e6
Joshua Brooks <josh.vdbroek@gmail.com>
2018-10-30 14:36:36 +0800

    modify docker-compose, docker-cmd, buildscript

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+HOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')
+FILE=python3.Dockerfile
+container=oipa
+
+docker build --build-arg HOST=${HOST} -t ${container} -f ${FILE} .
